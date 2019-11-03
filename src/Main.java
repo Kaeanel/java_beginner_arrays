9a3e2a3e75a9cbc5fc9167c0854311a0b67dbe0f
Denis <denisfilipov.12@gmail.com>
2019-11-03 23:41:32 +0100

    Initial commit, starting to loop through an array

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Create a new friends array using the array literal shortcut.
+        // Include Ben, Alena, Pasan
+        String[] friends = {"Ben","Alena","Pasan"};
+        for (String friend : friends){
+
+        }
+    }
+}
