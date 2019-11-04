db6d51efb2827537df715637993158342b543206
Denis <denisfilipov.12@gmail.com>
2019-11-04 22:32:05 +0100

    added a for loop for multi-dimentional arrays

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,27 @@ public class Main {
                               i + 1,
                               bensScoreCard[i]);
         }
+        // Looping through a multidimentional array
+        int[][] scoreCards = {
+                // Ben's
+                {1, 2, 4, 2, 6, 5, 4, 3, 3, 2, 5, 7, 2, 7, 8, 4, 3, 2},
+                // Alena's
+                {2, 3, 5, 1, 1, 2, 3, 1, 1, 2, 4, 1, 3, 3, 2, 6, 3, 2},
+                // Pasan's
+                {4, 4, 2, 1, 2, 2, 1, 4, 2, 2, 2, 3, 2, 5, 8, 1, 2, 2}
+        };
+        // for each friend
+        for (int i = 0; i < friends.length; i++){
+            System.out.printf("%s's scores%n-----------------------------%n",
+                              friends[i]);
+            // for each hole
+            for (int j = 0; j < scoreCards[i].length;j++){
+                System.out.printf("Hole %d: %d%n",
+                                  j + 1,
+                                  scoreCards[i][j]);
+            }
+
+        }
 
     }
 }
